38de9df61d3aab4012978a6cdb0b1938265dadb7
Himanshu <himanshukmr6@gmail.com>
2020-05-10 13:24:31 +0530

    Demo Application for Asynchronous Communication by ActiveMQ

---
+++ src/main/java/com/asynchronousCommunication/demoOfasynchronousCommunication/DemoOfasynchronousCommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.asynchronousCommunication.demoOfasynchronousCommunication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOfasynchronousCommunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOfasynchronousCommunicationApplication.class, args);
+	}
+
+}
